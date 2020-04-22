f22d4c2beba1a79fd688567f3b663ace8b4f819a
Alvaro Iribarren <airibarren@fi.uba.ar>
2020-04-22 03:42:43 -0300

    Dale que falta poco

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ const port = process.env.PORT || 5000
 server.listen(port, ()=>{
     console.log("Servidor escuchando en el puerto:", port);
 });
+
+server.get("/", (req, res)=> {
+    res.render("index.ejs")
+})
+
+server.get("/login", (req, res) =>{
+    res.render("login.ejs")
+})
+
+server.get("/saludo", (req, res) => {
+    res.render("saludo.ejs")
+})
+
+server.get("*", (req, res) => {
+    res.end("Todavia el virgo del desarrollador no creo esta pestaña")
+})
