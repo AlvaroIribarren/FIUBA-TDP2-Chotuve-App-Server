41119ac35b4de88c4d63d6ea014587abff555946
Alvaro Iribarren <airibarren@fi.uba.ar>
2020-04-23 20:27:34 -0300

    Postgres-heroku

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ const unknownRoute = require("./routes/unknown")
 const bodyParser = require("body-parser")
 const server = express();
 
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
+
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: false}));
+
 
 const port = process.env.PORT || 5000
 server.listen(port, ()=>{
