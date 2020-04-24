730c63e65ca57ab8e94c4f55252005241dba0a21
Alvaro Iribarren <airibarren@fi.uba.ar>
2020-04-23 21:02:22 -0300

    asd

--- index.js
+++ index.js
@@ -7,27 +7,21 @@ const unknownRoute = require("./routes/unknown")
 const bodyParser = require("body-parser")
 const server = express();
 
-const { Client } = require('pg');
-
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-        console.log(JSON.stringify(row));
-    }
-    client.end();
-});
-
-
+// //----------------------------
+// var pg = require('pg');
+//
+// var connectionString = "postgres://xovbegcheqegut:13cd6d27773e4f49fbbc6fb4677a84d3b92f9c0093793238f468eab55e32186e@ec2-52-6-143-153.compute-1.amazonaws.com:5432/d6o8g7k6fm0qv1"
+//
+// pg.connect(connectionString, function(err, client, done) {
+//     client.query('SELECT * FROM your_table', function(err, result) {
+//         done();
+//         if(err) return console.error(err);
+//         console.log(result.rows);
+//     });
+// });
+// //----------------------------
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: false}));
-
 
 const port = process.env.PORT || 5000
 server.listen(port, ()=>{
