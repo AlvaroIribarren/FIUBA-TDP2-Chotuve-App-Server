af69cf2c1408cdade171228755c262f5f333b16c
Alvaro Iribarren <airibarren@fi.uba.ar>
2020-04-27 23:50:26 -0300

    SACANDO ID

--- routes/users.js
+++ routes/users.js
@@ -45,7 +45,6 @@ router.get("/:id/friends", async (req, res) => {
 
 function validateUser(body){
     const schema = {
-        id: Joi.number().required(),
         name: Joi.string().min(minNameLength).required(),
         password: Joi.string().min(minPassLength).required(),
         email: Joi.string().required(),
